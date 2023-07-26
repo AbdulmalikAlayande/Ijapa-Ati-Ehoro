ee4a09d769ab7ef8d7c6474e29773eed774fc0ce
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-07-26 10:00:26 +0100

    initial commit

---
+++ src/turtleandhare/RaceEndsException.java
@@ -0,0 +1,8 @@
+package turtleandhare;
+
+public class RaceEndsException extends RuntimeException{
+	
+	public RaceEndsException(String message){
+		super(message);
+	}
+}
--- test/turtleandhare/RaceCourseTest.java
+++ test/turtleandhare/RaceCourseTest.java
@@ -1,11 +1,14 @@
 package turtleandhare;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static turtleandhare.TortoiseMoveType.*;
 
 public class RaceCourseTest {
 	Tortoise ijapa;
@@ -30,11 +33,29 @@ public class RaceCourseTest {
 	
 	@Test void testThatTortoiseCanDoFastPlod(){
 		assertThat(ijapa.getPosition()).isEqualTo(BigInteger.ONE.intValue());
-		ijapa.move(TortoiseMoveType.FAST_PLOD);
+		ijapa.move(FAST_PLOD);
 		assertThat(ijapa.getPosition()).isEqualTo(BigInteger.valueOf(4).intValue());
 	}
 	
 	@Test void testThatTortoiseCanDoSlowPlod(){
+		assertThat(ijapa.getPosition()).isEqualTo(BigInteger.ONE.intValue());
+		ijapa.move(FAST_PLOD);
+		ijapa.move(FAST_PLOD);
+		ijapa.move(FAST_PLOD);
+		ijapa.move(SLIP);
+		assertThat(ijapa.getPosition()).isEqualTo(BigInteger.valueOf(4).intValue());
+	}
 	
+	@Test void testThatIfTortoiseSlipsAndPositionIsLesserThanSixPositionPositionIsSetToOne(){
+		assertThat(ijapa.getPosition()).isEqualTo(BigInteger.ONE.intValue());
+		ijapa.move(SLIP);
+		assertThat(ijapa.getPosition()).isEqualTo(BigInteger.ONE.intValue());
+	}
+	
+	@Disabled
+	@Test void testThatIfTortoisePositionIsAtTheEndOfTheRaceTrackAndTortoiseTriesToMoveExceptionIsThrownToAvoidRaceTrackArrayIndexOutOfBounds(){
+		assertThat(ijapa.getPosition()).isEqualTo(BigInteger.ONE.intValue());
+		ijapa.setPosition(69);
+		assertThatThrownBy(()->ijapa.move(FAST_PLOD)).isInstanceOf(RaceEndsException.class);
 	}
 }
